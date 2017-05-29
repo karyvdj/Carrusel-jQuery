6efc45114eac1314a5b49032e7fb38079fbac313
karyvdj <kaina51@hotmail.com>
2017-05-29 16:16:47 -0500

    Dots de slider funcionan al seleccionarlo y trabajando en la transicion automatica

--- assets/js/app.js
+++ assets/js/app.js
@@ -42,12 +42,10 @@ $("#btn5").click(function(){
     $("#btn5").css("background-color", "#C572AF");
 });
 
-
-
-$(document).ready(function(){
-  $('#carrusel div:gt(0)').hide(); //gt(0) seleccina todo los div a partir del numero 0
-    setInterval(function(){
-    $('#carrusel div:first-child').fadeOut(0)
-         .next('div').fadeIn(1000)
-         .end().appendTo('#carrusel');}, 3000);
-});
+// $(document).ready(function(){
+//   $('#carrusel div:gt(0)').hide(); //gt(0) seleccina todo los div a partir del numero 0
+//     setInterval(function(){
+//     $('#carrusel div:first-child').fadeOut(0)
+//          .next('div').fadeIn(1000)
+//          .end().appendTo('#carrusel');}, 3000);
+// });
